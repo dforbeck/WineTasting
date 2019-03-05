eca105a9fd1d3132913e41fb9eae78d8c3164ab7
Debbie <debbieforbeck@gmail.com>
2019-03-05 13:52:56 -0500

    Add project files.

---
+++ WineTasting.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WineTasting.WebMVC.Startup))]
+namespace WineTasting.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
