a2f410ff90b52140504dd8ac50d4a2d664cd37da
Debbie <debbieforbeck@gmail.com>
2019-03-28 16:13:14 -0400

    set up base Tasting controller

--- WineTasting.WebApi/Controllers/TastingController.cs
+++ WineTasting.WebApi/Controllers/TastingController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WineTasting.Models.Tasting;
 using WineTasting.Services;
 
 namespace WineTasting.WebApi.Controllers
@@ -12,11 +13,53 @@ namespace WineTasting.WebApi.Controllers
     [Authorize]
     public class TastingController : ApiController
     {
-        public IHttpActionResult Get()
+        public IHttpActionResult GetAll()
         {
             TastingService tastingService = CreateTastingService();
             var tastings = tastingService.GetTastings();
             return Ok(tastings);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            TastingService tastingService = CreateTastingService();
+            var tasting = tastingService.GetTastingById(id);
+            return Ok(tasting);
+        }
+
+        public IHttpActionResult Post(TastingCreate tasting)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateTastingService();
+
+            if (!service.CreateTasting(tasting))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Put(TastingEdit tasting)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateTastingService();
+
+            if (!service.UpdateTasting(tasting))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateTastingService();
+
+            if (!service.DeleteTasting(id))
+                return InternalServerError();
+            return Ok();
         }
 
         private TastingService CreateTastingService()
